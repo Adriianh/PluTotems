c2163e651309cd60273ca2be2f7362ac186bbb94
Adriianh <adrianfunez979@gmail.com>
2024-11-25 20:02:10 -0600

    fix: totem item can be placed

--- project/module-core/src/main/kotlin/com/github/adriianh/core/event/PlayerPlaceEvent.kt
+++ project/module-core/src/main/kotlin/com/github/adriianh/core/event/PlayerPlaceEvent.kt
@@ -1,6 +1,7 @@
 package com.github.adriianh.core.event
 
 import com.github.adriianh.common.totem.TotemExecutor.handleEvent
+import com.github.adriianh.common.totem.TotemFactory
 import org.bukkit.event.block.BlockPlaceEvent
 import taboolib.common.platform.event.SubscribeEvent
 
@@ -9,6 +10,9 @@ object PlayerPlaceEvent {
     fun onPlayerPlaceEvent(event: BlockPlaceEvent) {
         val player = event.player
         val item = event.itemInHand
+
+        if (!TotemFactory.isTotem(item)) return
+        event.isCancelled = true
 
         handleEvent(player, item, "placeable", event, true)
     }
